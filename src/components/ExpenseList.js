b29209549b3afb70292bb344645059af34480783
Mohammed Waseem <waseem2001002@gmail.com>
2022-09-04 20:08:30 +0300

    second commit

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,14 +3,6 @@ import ExpenseItem from './ExpenseItem';
 import {AppContext} from '../content/AppContext';
 function ExpenseList() {
     const state = useContext(AppContext);
-    const exp = [
-        {
-            id:1,name:'shopping',cost:10,
-        },
-        {
-            id:2,name:'eating',cost:20,
-        },
-    ]
     return (
         <ul className='list-group'>
             {state.expenses.map((expense) => <ExpenseItem expense={expense}/>)}
